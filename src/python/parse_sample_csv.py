60f358c68db4867c30db3601bcc91ff3ff8e8e42
m006703 <sakai.yuta@mayo.edu>
2020-07-17 13:07:24 -0500

    working function to create the aggregate result file

--- src/python/parse_sample_csv.py
+++ src/python/parse_sample_csv.py
@@ -23,16 +23,52 @@ def main():
         "-o", dest="output_directory", required=True,
         help="Full path to the directory to save the result file"
     )
+    parser.add_argument(
+        "-f", dest="metric_file_path", required=True,
+        help="Full path to the metrics.csv file"
+    )
 
     args = parser.parse_args()
 
     input_directory = os.path.normpath(args.input_directory)
     output_directory = os.path.normpath(args.output_directory)
+    metric_file_path = os.path.abspath(args.metric_file_path)
 
     # Parse the csv file
-    result_dict = parse_pgdx_csv(input_directory)
+    result_dict, sample_list = parse_pgdx_csv(input_directory)
 
-    print(result_dict)
+    # Create aggregate result csv file
+    make_aggregate_result_file(metric_file_path, output_directory, result_dict, sample_list)
+
+
+def make_aggregate_result_file(metric_file_path, output_directory, result_dict, sample_list):
+    """
+    Aggregate the desired metrics and output into a csv file
+    :param metric_file_path:
+    :param output_directory:
+    :param result_dict:
+    :param sample_list:
+    :return:
+    """
+    result_file = open(output_directory + "/elio_plasma_aggregate_results.csv", "w")
+    # Make the header row with sample name
+    for sample in sample_list:
+        result_file.write("," + sample)
+    result_file.write("\n")
+    # Open the metric file to see which metrics needs to be written
+    metric_file = open(metric_file_path, "r")
+    for line in metric_file:
+        line = line.rstrip()
+        line_item = line.split(",")
+        metric_header = line_item[0]
+        metric_category = line_item[1]
+        metric = line_item[2]
+        result_file.write(metric_header)
+        for sample in sample_list:
+            result_file.write("," + result_dict[sample][metric_category][metric])
+        result_file.write("\n")
+    result_file.close()
+    metric_file.close()
 
 
 def parse_pgdx_csv(input_directory):
@@ -42,12 +78,14 @@ def parse_pgdx_csv(input_directory):
     :return:
     """
     result_dict = {}
+    sample_list = []
     csv_file_list = os.listdir(input_directory)
     for file in csv_file_list:
         sample_name = file.split(".")[0]
         csv_file = open(input_directory + "/" + file, "r")
         if sample_name not in result_dict.keys():
             result_dict[sample_name] = {}
+            sample_list.append(sample_name)
         else:
             print("Sample: " + sample_name + " has already been analyzed. Exiting script")
             sys.exit()
@@ -73,7 +111,7 @@ def parse_pgdx_csv(input_directory):
                 # Add all the items underneath the header inside each header key
                 result_dict[sample_name][header][line_item[0]] = line_item[1]
         csv_file.close()
-    return result_dict
+    return result_dict, sample_list
 
 
 if __name__ == "__main__":
